e2881ea8451277547dc8045464b8731798b328b5
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2023-12-17 23:21:07 +0700

    Change idea into another way

--- LengthOfLastWord.cpp
+++ LengthOfLastWord.cpp
@@ -1,5 +1,26 @@
 #include<stdio.h>
-char findLastWord(char fullWord);
-int countChar(int count, char fullWord);
-int changeWordToArr(int count, char fullWord);
-int lengthOfWord(int count, char fullWord);
+#include<string.h>
+int lengthOfLastWord(const char* fullWord)
+{
+    int length = 0;
+    int index = strlen(fullWord) - 1;
+    while (index >= 0 && fullWord[index] == ' ')
+    {
+        index--;
+    }
+    while (index >= 0 && fullWord[index] == ' ')
+    {
+        length++;
+        index--;
+    }
+    return length;
+}
+int main()
+{
+    char inputString[100];
+    printf("Enter the string: ");
+    fgets(inputString, sizeof(inputString), stdin);
+    int result = lengthOfLastWord(inputString);
+    printf("Length of last word is: ");
+    printf("%d", result);
+}
