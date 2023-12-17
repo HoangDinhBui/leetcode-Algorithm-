10a94410f7747c2b296e78428380193eff2f3013
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2023-12-17 23:08:29 +0700

    Setup function name from idea

---
+++ LengthOfLastWord.cpp
@@ -0,0 +1,5 @@
+#include<stdio.h>
+char findLastWord(char fullWord);
+int countChar(int count, char fullWord);
+int changeWordToArr(int count, char fullWord);
+int lengthOfWord(int count, char fullWord);
