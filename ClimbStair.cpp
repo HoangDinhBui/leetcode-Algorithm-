285dc35c3b276e0f0ec295ce9b5d97fece4c0a6a
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2023-12-18 16:18:21 +0700

    Upload clean code of algorithm about climb stair

--- ClimbStair.cpp
+++ ClimbStair.cpp
@@ -2,8 +2,6 @@
 //70. Climbing stair
 int main()
 {
-    int stepNum1 = 1;
-    int stepNum2 = 2;
     int numOfSideStep;
     printf("Enter the number of sidestep: ");
     scanf("%d", &numOfSideStep);
@@ -13,12 +11,10 @@ int main()
         return 0;
     }
     int dp[numOfSideStep];
-    dp[0] = 1;
-    dp[1] = 1;
-    dp[2] = 2;
+    dp[0] = 1, dp[1] = 1, dp[2] = 2;
     for (int i = 3; i <= numOfSideStep; i++)
     {
         dp[i] = dp[i - 1] + dp[i - 2];//the next step is equal to the 1st previous step and 2nd previous step
     }
-    printf("The number of way to go up stair: %d", dp[numOfSideStep - 1]);
+    printf("The number of way to go up stair: %d", dp[numOfSideStep]);
 }
