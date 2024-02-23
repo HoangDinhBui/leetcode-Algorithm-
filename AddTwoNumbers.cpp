02e9aee4ad235b51502cf4194b41ffebc4f6352d
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-02-23 15:46:24 +0700

    Complete this algorithm

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -4,13 +4,29 @@ using namespace std;
 
 void addTwoNums(int listNode1[], int listNode2[], int numOfDigits)
 {
-    int *sumOfListNode = new int;
-    for (int i = numOfDigits - 1; i >= 0; i--)
+    int *sumOfListNode = new int[numOfDigits];
+    int firstNum = 0, secondNum = 0, addNum;
+    for(int i = numOfDigits - 1; i >= 0; i--)
     {
-        sumOfListNode[i] = listNode1[i] + listNode2[i];
+        firstNum = firstNum * 10;
+        firstNum = firstNum + listNode1[i];
     }
+    for(int i = numOfDigits - 1; i >= 0; i--)
+    {
+        secondNum = secondNum * 10;
+        secondNum = secondNum + listNode2[i];
+    }
+   
+    addNum = firstNum + secondNum;
+
     for (int i = 0; i < numOfDigits; i++)
-        cout << sumOfListNode[i] << ", ";
+    {
+        sumOfListNode[i] = addNum % 10;
+        addNum = addNum / 10;
+    }
+    for (int i = numOfDigits - 1; i >= 0; i--)
+        cout << sumOfListNode[i] << " ";
+    delete[] sumOfListNode; 
 }
 
 void input(int listNode[], int numOfDigits)
@@ -31,4 +47,6 @@ int main()
     input(listNode2, numOfDigits);
     cout << "Result: ";
     addTwoNums(listNode1, listNode2, numOfDigits);
+    delete[] listNode1;
+    delete[] listNode2;
 }
