57f887b2a8e828c1b6bdedf9f42f6a84763503bc
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-03-01 10:13:35 +0700

    upload algorithm code using branch and bound tech

--- AddBinary2.cpp
+++ AddBinary2.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void slpitNum(int n, int *binary, int &count)
+void splitNum(int n, int *binary, int &count)
 {
     int i = 0;
     while(n > 0)
@@ -14,9 +14,9 @@ void slpitNum(int n, int *binary, int &count)
     count = i;
 }
 
-void sumBinary(int *binaryA, int *binaryB, int *binarySum, int count1, int count2)
+void sumBinary(int *binaryA, int *binaryB, int *binarySum, int count)
 {
-    for(int i = count1 + count2 - 1; i < count1 + count2; i--)
+    for(int i = count - 1; i > 0; i--)
     {
         if(binaryA[i] == 0 && binaryB[i] == 1)
         {
@@ -35,11 +35,12 @@ void sumBinary(int *binaryA, int *binaryB, int *binarySum, int count1, int count
         }
         if(binaryA[i] == 1 && binaryB[i] == 1)
         {
-            binarySum[i] = 0;
-            //sumBinary(binaryA, binaryB, binarySum, count1, count2);
-            if(binarySum[i-1] == 0)
-                binarySum[i]++;
-            if(binarySum[i-1] == 1)
+            if(binaryA[i-1] == 1 && binaryB[i-1] == 1)
+            {
+                binarySum[i] = 0;
+                binarySum[i-1] = 1;
+            }
+            else
                 binarySum[i] = 1;
         }
     }
@@ -48,14 +49,17 @@ void sumBinary(int *binaryA, int *binaryB, int *binarySum, int count1, int count
 int main()
 {
     int num1, num2, count1 = 0, count2 = 0;
-    cout << "Enter two number need to add:";
+    cout << "Enter two number need to add: ";
     cin >> num1 >> num2;
-    int *binaryA = new int[10];
-    int *binaryB = new int[10];
-    int *binarySum = new int[10];
-    slpitNum(num1, binaryA, count1);
-    slpitNum(num2, binaryB, count2);
-    sumBinary(binaryA, binaryB, binarySum, count1, count2);
+    int binaryA[10] = {0}; //= new int[10];
+    int binaryB[10] = {0}; //= new int[10];
+    int binarySum[10] = {0}; //= new int[10];
+    splitNum(num1, binaryA, count1);
+    splitNum(num2, binaryB, count2);
+    if(count1 > count2)
+        sumBinary(binaryA, binaryB, binarySum, count1);
+    else
+        sumBinary(binaryA, binaryB, binarySum, count2);
     for(int i = 0; i < count1+count2; i++)
     {
         cout << binarySum[i] << " ";
