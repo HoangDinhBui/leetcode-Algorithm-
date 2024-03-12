6364fa79173d41304b46cc17e1c0c2511de69e93
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-03-12 08:48:49 +0700

    Upload CodeForce algorithm code about an array can change all elements into 0 by using a givem rules

--- RudoltAnd121.cpp
+++ RudoltAnd121.cpp
@@ -2,50 +2,42 @@
 
 using namespace std;
 
-void equalZero(int *A, int n)
-{
-    int i;
-    
-        while(A[i] != 0)
-        {
-            for(i = n - 1; i >= 2; i--)
-            {
-            A[i-1] -= 1;
-            A[i] -= 2;
-            A[i+1] -= 1;
-            }
-        
+void makeZero(int A[], int n) {
+    for (int i = 1; i < n - 1; i++) {
+        if (A[i] > 0) {
+            int reduction = min(A[i], min(A[i - 1], A[i + 1]));
+            A[i - 1] -= reduction;
+            A[i] -= reduction * 2;
+            A[i + 1] -= reduction;
         }
-}
-void result()
-{
-    int n, i, count = 0;
-    cin >> n;
-    int *A = new int[n+1];
-    for(i = 1; i <= n; i++)
-    {
-        cin >> A[i];
     }
-    equalZero(A, n);
-    for(i = 1; i <= n; i++)
-    {
-        if(A[i] == 0)
-            count++;
-    }
-    if(count == n)
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
-    delete[] A;
 }
 
-int main()
-{
-    int m; 
+bool allZero(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (A[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int m;
     cin >> m;
-    while(m--)
-    {
-        result();
+    while (m--) {
+        int n;
+        cin >> n;
+        int A[200000];
+        for (int i = 0; i < n; i++) {
+            cin >> A[i];
+        }
+        makeZero(A, n);
+        if (allZero(A, n)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
