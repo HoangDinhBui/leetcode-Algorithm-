037a03047ee05c507a75ae7090b4df5f7c6f8d95
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-04-02 23:47:36 +0700

    Changing the code by using class and Linked List to reduce time complexity

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -2,51 +2,73 @@
 
 using namespace std;
 
-void addTwoNums(int listNode1[], int listNode2[], int numOfDigits)
-{
-    int *sumOfListNode = new int[numOfDigits];
-    int firstNum = 0, secondNum = 0, addNum;
-    for(int i = numOfDigits - 1; i >= 0; i--)
-    {
-        firstNum = firstNum * 10;
-        firstNum = firstNum + listNode1[i];
-    }
-    for(int i = numOfDigits - 1; i >= 0; i--)
-    {
-        secondNum = secondNum * 10;
-        secondNum = secondNum + listNode2[i];
-    }
-   
-    addNum = firstNum + secondNum;
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-    for (int i = 0; i < numOfDigits; i++)
-    {
-        sumOfListNode[i] = addNum % 10;
-        addNum = addNum / 10;
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* answer = new ListNode();
+        ListNode* tmp = answer;
+        int carry = 0;
+        while(l1 != nullptr || l2 != nullptr || carry){
+            int num1 = (l1 != nullptr ? l1->val : 0);
+            int num2 = (l2 != nullptr ? l2->val : 0);
+            l1 = (l1 != nullptr ? l1 -> next : nullptr);
+            l2 = (l2 != nullptr ? l2 -> next : nullptr);
+            int sum = num1 + num2 + carry;
+            carry = sum / 10;
+            ListNode *newNode = new ListNode(sum % 10);
+            tmp-> next = newNode;
+            tmp = tmp -> next;
+        }
+        return answer -> next;
     }
-    for (int i = numOfDigits - 1; i >= 0; i--)
-        cout << sumOfListNode[i] << " ";
-    delete[] sumOfListNode; 
+};
+
+// Utility function to print a linked list
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        cout << current->val << " -> ";
+        current = current->next;
+    }
+    cout << "nullptr" << endl;
 }
 
-void input(int listNode[], int numOfDigits)
-{
-    for(int i = 0; i < numOfDigits; i++)
-        cin >> listNode[i];
-}
-int main()
-{
-    int numOfDigits;
-    cout << "Enter number of digits: ";
-    cin >> numOfDigits;
-    int *listNode1 = new int;
-    int *listNode2 = new int;
-    cout << "Enter the element of first number: ";
-    input(listNode1, numOfDigits);
-    cout << "Enter the element of second number: ";
-    input(listNode2, numOfDigits);
-    cout << "Result: ";
-    addTwoNums(listNode1, listNode2, numOfDigits);
-    delete[] listNode1;
-    delete[] listNode2;
+int main() {
+    // Creating first list: 2 -> 4 -> 3
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+    
+    // Creating second list: 5 -> 6 -> 4
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    Solution solution;
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+
+    cout << "List 1: ";
+    printList(l1);
+
+    cout << "List 2: ";
+    printList(l2);
+
+    cout << "Sum: ";
+    printList(result);
+
+    // Clean up memory
+    delete l1;
+    delete l2;
+    delete result;
+
+    return 0;
 }
