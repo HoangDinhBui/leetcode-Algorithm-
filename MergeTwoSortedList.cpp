7f6964f87ac0e6cc1ce13d7d7f01dcd7a426e7fe
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-04-04 00:19:15 +0700

    Change algorithm of merge two array by using linked list and recursion

--- MergeTwoSortedList.cpp
+++ MergeTwoSortedList.cpp
@@ -1,67 +1,77 @@
-#include <stdio.h>
-//Function of sort merge Array
-void sort(int mergeArr[], int numOfMergeArr)
-{
-    for(int i = 0; i < numOfMergeArr - 1; i++)
-    {
-        for(int j = i + 1; j < numOfMergeArr; j++)
-        {
-            if(mergeArr[i] > mergeArr[j])
-            {
-                int tmp = mergeArr[i];
-                mergeArr[i] = mergeArr[j];
-                mergeArr[j] = tmp;
+#include <iostream>
+
+using namespace std;
+
+// Định nghĩa cấu trúc cho node trong danh sách liên kết
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // Không cần khởi tạo biến third ở đây
+        ListNode* dummy = new ListNode(); // Node giả định, không chứa giá trị thực sự
+        ListNode* last = dummy;
+        
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                last->next = list1;
+                list1 = list1->next;
+            } else {
+                last->next = list2;
+                list2 = list2->next;
             }
+            last = last->next;
         }
+        
+        // Nối phần còn lại của danh sách chưa được hợp nhất
+        last->next = list1 ? list1 : list2;
+        
+        // Trả về danh sách mới (bỏ qua node giả định)
+        return dummy->next;
     }
+};
+
+// Hàm để in danh sách liên kết
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        cout << current->val << " ";
+        current = current->next;
+    }
+    cout << endl;
 }
-//Function of merge two array
-void mergeArr(int sortedArr1[], int sortedArr2[], int numOfArr1, int numOfArr2)
-{
-    int index = 0;
-    int numOfMergeArr = numOfArr1 + numOfArr2;
-    int mergeArr[numOfMergeArr];
-    // Copy elements from sortedArr1 to mergeArr
-    for(int i = 0; i < numOfArr1; i++)
-    {
-        mergeArr[index] = sortedArr1[i];
-        index++;
-    }
-    // Copy elements from sortedArr2 to mergeArr
-    for(int i = 0; i < numOfArr2; i++)
-    {
-        mergeArr[index] = sortedArr2[i];
-        index++;
-    }
-    //Sort merge array
-    sort(mergeArr, numOfMergeArr);
-    //Print sorted merge array
-    printf("After merge: ");
-    for(int i = 0; i < numOfMergeArr; i++)
-    {
-        printf("%d ", mergeArr[i]);
-    }
-}
-int main()
-{
-    int numOfArr1, numOfArr2;
-    //Setup 1st Array
-    printf("Enter the number of 1st sorted array: ");
-    scanf("%d", &numOfArr1);
-    int sortedArr1[numOfArr1];
-    printf("Enter the 1st array's element: ");
-    for(int i = 0; i < numOfArr1; i++)
-    {
-        scanf("%d", &sortedArr1[i]);
-    }
-    //Setup 2nd Array
-    printf("Enter the number of 2nd sorted array: ");
-    scanf("%d", &numOfArr2);
-    int sortedArr2[numOfArr2];
-    printf("Enter the 2nd array's element: ");
-    for(int i = 0; i < numOfArr2; i++)
-    {
-        scanf("%d", &sortedArr2[i]);
-    }
-    mergeArr(sortedArr1, sortedArr2, numOfArr1, numOfArr2);
+
+int main() {
+    // Tạo danh sách liên kết thứ nhất: 1 -> 3 -> 5
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(3);
+    list1->next->next = new ListNode(5);
+
+    // Tạo danh sách liên kết thứ hai: 2 -> 4 -> 6
+    ListNode* list2 = new ListNode(2);
+    list2->next = new ListNode(4);
+    list2->next->next = new ListNode(6);
+
+    // Tạo một đối tượng của lớp Solution
+    Solution solution;
+    
+    // Hợp nhất hai danh sách và lưu kết quả vào một biến mới
+    ListNode* mergedList = solution.mergeTwoLists(list1, list2);
+    
+    // In danh sách đã hợp nhất
+    cout << "Merged List: ";
+    printList(mergedList);
+
+    // Giải phóng bộ nhớ
+    delete list1;
+    delete list2;
+    delete mergedList;
+
+    return 0;
 }
