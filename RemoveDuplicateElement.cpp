c7389f03d4206867136ce642696aee83f47c707b
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-04-05 21:43:59 +0700

    Change code into class and reduce time complexity

--- RemoveDuplicateElement.cpp
+++ RemoveDuplicateElement.cpp
@@ -1,31 +1,39 @@
-#include <stdio.h>
-int main()
-{
-    int numOfElement;
-    printf("Enter the number of element of sorted array: ");
-    scanf("%d", &numOfElement);
-    int sortedArr[numOfElement];
-    printf("Enter the element of sorted array: ");
-    for(int i = 0; i < numOfElement; i++)
-    {
-        scanf("%d", &sortedArr[i]);
-    }
-    // Remove duplicates by replacing them with -1
-    for(int i = 0; i < numOfElement - 1; i++)
-    {
-        for(int j = i + 1; j < numOfElement; j++)
-        {
-            if(sortedArr[i] == sortedArr[j])
-            {
-                sortedArr[j] = -1;
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 1;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
             }
         }
+        return j;
     }
-    // Print the modified array without duplicates
-    for(int i = 0; i < numOfElement; i++)
-    {
-        if(sortedArr[i] != -1)
-            printf("%d ", sortedArr[i]);
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9};
+    
+    cout << "Original array: ";
+    for (int num : nums) {
+        cout << num << " ";
     }
+    cout << endl;
+    
+    int newLength = sol.removeDuplicates(nums);
+    
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < newLength; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
