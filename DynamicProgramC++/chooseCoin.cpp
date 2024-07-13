ad8c9d26c0f2acdad7148c24b2cb02a074341f5d
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-07-13 23:34:29 +0700

    Edit Dynamic Program algorithm

--- DynamicProgramC++/chooseCoin.cpp
+++ DynamicProgramC++/chooseCoin.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+#include <vector>
+#define MOD 1000000007
 
 using namespace std;
 
 int main() {
     int n, S;
     cin >> n >> S;
-    int a[n];   
+    vector<int> a(n);
     for(int i = 0; i < n; i++) 
         cin >> a[i];
-    int dp[S + 1];
-    for(int i = 0; i < S + 1; i++)
-        dp[i] = 0;
-    dp[0] = 1; //Có 1 cách để tạo ra tổng số xu bằng 0
-    for(int i = 0; i < n; i++) {
-        for(int j = S; j >= a[i]; j--) {
-            if(dp[j - a[i]] == 1)
-                dp[j] = 1;
+    
+    vector<int> dp(S + 1, 0);
+    dp[0] = 1; // Có 1 cách để tạo ra tổng bằng 0 là không chọn đồng xu nào
+
+    for(int j = 1; j <= S; j++) {
+        for(int i = 0; i < n; i++) {
+            if (j >= a[i]) {
+                dp[j] = (dp[j] + dp[j - a[i]]) % MOD;
+            }
         }
     }
+    
     cout << dp[S] << endl;
+    return 0;
 }
