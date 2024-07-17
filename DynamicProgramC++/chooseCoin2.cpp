5ae7832069166367a2889844c2085c4173be22ed
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2024-07-17 23:57:15 +0700

    Dynamic Program

--- DynamicProgramC++/chooseCoin2.cpp
+++ DynamicProgramC++/chooseCoin2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #define MOD 1000000007
- 
+
 using namespace std;
- 
+
 int main() {
     int n, S;
     cin >> n >> S;
@@ -13,12 +13,10 @@ int main() {
     
     vector<int> dp(S + 1, 0);
     dp[0] = 1; // Có 1 cách để tạo ra tổng bằng 0 là không chọn đồng xu nào
- 
-    for(int j = 1; j <= S; j++) {
-        for(int i = 0; i < n; i++) {
-            if (j >= a[i]) {
-                dp[j] = (dp[j] + dp[j - a[i]]) % MOD;
-            }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = a[i]; j <= S; j++) {
+            dp[j] = (dp[j] + dp[j - a[i]]) % MOD;
         }
     }
     
