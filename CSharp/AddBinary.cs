de37952cf25d0883629f6b1cb800a75f7267f977
HoangBD <buidinhhoang1910@gmail.com>
2024-10-22 07:53:28 +0700

    Add main function to complete program

--- CSharp/AddBinary.cs
+++ CSharp/AddBinary.cs
@@ -1,19 +1,36 @@
+using System;
+using System.Text;
+
 public class Solution {
     public string AddBinary(string a, string b) {
         StringBuilder res = new StringBuilder();
         int i = a.Length - 1;
         int j = b.Length - 1;
         int carry = 0;
-        while(i >= 0 || j >= 0) {
+
+        while (i >= 0 || j >= 0) {
             int sum = carry;
-            if(i >= 0) sum += a[i--] - '0';
-            if(j >= 0) sum += b[j--] - '0';
+
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
             res.Append(sum % 2);
             carry = sum / 2;
         }
-        if(carry > 0) res.Append('1');
+
+        if (carry > 0) res.Append('1');
+
         char[] resultArray = res.ToString().ToCharArray();
         Array.Reverse(resultArray);
+
         return new string(resultArray);
+    }
+
+    public static void Main(string[] args) {
+        Solution solution = new Solution();
+        string a = "1010";
+        string b = "1011";
+        string result = solution.AddBinary(a, b);
+        Console.WriteLine(result);
     }
 }
