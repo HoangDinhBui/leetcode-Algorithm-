9c02c12136bf9912766d72fc319e35c608cfa9b3
HoangBD <buidinhhoang1910@gmail.com>
2024-11-05 10:12:10 +0700

    Add: Practice Tree

--- CSharp/MinDepthOfTree.cs
+++ CSharp/MinDepthOfTree.cs
@@ -1,17 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-public class Solution {
+
+ /* Definition for a binary tree node.*/
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ public class Solution {
     public int MinDepth(TreeNode root) {
         if(root == null) return 0;
         if (root.left == null) return 1 + MinDepth(root.right);
--- CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
+++ CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d131c75258c7a0457bff64fb15ab54d5655c50a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8765755c17207b42206a58f12364a98501201a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
